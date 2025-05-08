713e75e6799bf8025cf9c79c22f56d4895f80a27
amprajasri <amprajasri@gmail.com>
2025-05-08 10:00:00 +0530

    day2 resources uploaded

---
+++ hello-messaging-app/src/test/java/com/example/hellomessagingapp/HelloMessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellomessagingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMessagingAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
